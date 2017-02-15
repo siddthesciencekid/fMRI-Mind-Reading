e60768ccbd56604c53bf5a80a0002a7ccc4300ce
Siddhartha Gorti <sidd.gorti@hotmail.com>
2017-02-14 16:39:54 -0800

    Added inital files

---
+++ fmri_solver.py
@@ -0,0 +1,12 @@
+# Siddhartha Gorti & Shilpa Kumar
+# FMRI Project
+# CSE 446 Machine Learning
+# WINTER 2017
+
+import scipy.io
+
+def main():
+    print "Hello World"
+
+if __name__ == "__main__":
+    main()
