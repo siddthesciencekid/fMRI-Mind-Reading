20f06235a331bef11c596b204cb03ace204daecb
shilpa kumar <shilpak@cs.washington.edu>
2017-02-14 16:55:52 -0800

    trying to read in the files, training signals not working

--- fmri_solver.py
+++ fmri_solver.py
@@ -6,7 +6,14 @@
 import scipy.io
 
 def main():
-    print "Hello World"
+    signalsTest = scipy.io.mmread("data/subject1_fmri_std.test.mtx")
+    signalsTrain = scipy.io.mmread("data/subject1_fmri_std.train.mtx")
+    #train not working
+    print("hello???")
+    wordsTest = scipy.io.mmread("data/subject1_wordid.test.mtx")
+    wordsTrain = scipy.io.mmread("data/subject1_wordid.train.mtx")
+    semanticFeatures = scipy.io.mmread("data/word_feature_centered.mtx")
+    print(wordsTest.shape)
 
 if __name__ == "__main__":
     main()
