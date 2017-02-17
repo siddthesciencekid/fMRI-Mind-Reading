1309b1a205c83505810ae6dde13281273efaabb7
shilpa kumar <shilpak@cs.washington.edu>
2017-02-17 02:59:08 -0800

    More detailed comments added

--- fmri_solver.py
+++ fmri_solver.py
@@ -112,18 +112,28 @@ def scd(lmbda, y, X, w, z, num_iterations):
             # Pick a random j from 0 to 2*num_attributes - 1
             j = randint(0, (num_attributes * 2) - 1)
 
+            # ** Since w~ is [w_plus; w_minus], and X~ is [X, -X], we save a lot of space by doing the algorithm in terms of just X without replicating
+            # it. So, to account for this, we split w~ into w_plus and w_minus. 
+            # We have two cases: where j is in the 'left half', and where it is in the 'right half'. When it is in the left half, 
+            # j < num_attributes, and we update w_plus. In the other case, we update w_minus, and because it is -X, the gradient's sign is flipped. **
+
+            # ** We could use w = w_plus - w_minus and compute the dot product for the gradient every time, since
+            # we update w_plus and w_minus after every random pick of j. However, computing the dot product every single
+            # run of every iteration takes very long. So we will keep track of w_transpose * X, and when we update w_plus and w_minus,
+            # we either add w_plus * x_j or subtract w_minus * x_j >> leads to the same thing much faster. **
+
+
             # Left half so w(plus)
             if (j < num_attributes):
                 # Get jth column
                 x_j = X[:, j]
-                # Gradient = 1/n * x_j * (xw(plus) - y) + lambda
+                # Gradient = 1/n * x_j * (xw~ - y) + lambda
                 diff = np.subtract(wTX, y)
                 gradient = np.dot(x_j, diff) / num_values * 1.0 + lmbda
-                # Max of -w~[j] and -gradient. Since j < num_attributes, w~ is w_plus 
+                # Max of -w~[j] and -gradient. Since j < num_attributes, update w_plus 
                 max_add = max(-w_plus[j], -gradient)
                 # Update w~[j] (in this case w_plus[j])
                 w_plus[j] += max_add
-                # Update wTX because w in this is w~, but we shouldn't do dot product every time
                 # Only one column of w changed so update x_j column with new weight and add to wTX
                 wTX = np.add(wTX, max_add * x_j)
 
@@ -133,19 +143,20 @@ def scd(lmbda, y, X, w, z, num_iterations):
                 j = j - num_attributes
                 # Get jth column
                 x_j = X[:, j]
-                # Gradient = -1/n * x_j * (xw(minus) - y) + lambda
-                diff = np.subtract(wTX, y)
-                gradient = -1.0 * np.dot(x_j, diff) / num_values + lmbda
-                # Max of -w~[j] and -gradient. Since j >= p, w~ is w_minus
+                # Gradient = 1/n * x_j * (y - w~X) + lambda
+                diff = np.subtract(y, wTX)
+                gradient = 1.0 * np.dot(x_j, diff) / num_values + lmbda
+                # Max of -w~[j] and -gradient. Since j >= p, update w_minus
                 max_add = max(-w_minus[j], -gradient)
                 # Update w~[j] (in this case w_minus[j])
                 w_minus[j] +=  max_add
-                # W~ = w_plus - w_minus. So to update completely, subtract correct amount
+                # W = w_plus - w_minus. So to update completely, subtract correct amount
                 # Only one column of w changed so update x_j column with new weight and subtract from wTX
                 wTX = np.subtract(wTX, max_add * x_j)
 
         current_iteration += 1
         # w~ = w_plus - w_minus
+        # We don't keep track of this earlier because it is not used for dot products bc of lack of efficiency.
         w = np.subtract(w_plus, w_minus)
 
         # Hit convergence before all the iterations are over
