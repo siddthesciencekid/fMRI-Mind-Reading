f38409cf04de1988b88b0e267b65b381c9cf6290
Siddhartha Gorti <sidd.gorti@hotmail.com>
2017-02-22 02:31:39 -0800

    Finished cleanup and added functions to plot semantic feature model fit data on one chart

--- fmri_solver.py
+++ fmri_solver.py
@@ -6,10 +6,9 @@
 import scipy.io
 import numpy as np
 import timeit
+import math
 
-from matplotlib import pyplot as plt_training
-from matplotlib import pyplot as plt_PGD
-from matplotlib import pyplot as plt_SCD
+from matplotlib import pyplot as plt
 from lasso import lasso
 from scd import scd
 from pgd import pgd
@@ -28,15 +27,38 @@ def main():
     
     semantic_features = scipy.io.mmread("data/word_feature_centered.mtx")
 
-    # TODO: Explain what is being called here
+    # Use these functions to plot model fit data (squared test & train error and num nonzero
+    # coefficients) about different lambda values for chosen semantic features for both
+    # SCD and PGD.
+
+    # NOTE: LAMBDA values are different across the two
+    # SET THE LAST PARAMETER TO FALSE TO PLOT MODEL FIT DATA USING SCD AND TRUE FOR PGD
+
+    '''
+    lambda_values_pgd = [.1, .5, 1, 5, 10, 20, 40, 100, 200]
+    lambda_values_scd = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, .4]
+
     plot_semantic_feature_squared_error(signals_test, signals_train,
-                                        words_train, words_train, semantic_features, True)
+                                        words_test, words_train, semantic_features,
+                                        lambda_values_scd, False)
+
+    plot_semantic_feature_squared_error(signals_test, signals_train,
+                                        words_test, words_train, semantic_features,
+                                        lambda_values_pgd, True)
+    '''
 
 
+# Plots model fit data (squared test & train error and num nonzero
+# coefficients) about different lambda values for chosen semantic features
 def plot_semantic_feature_squared_error(signals_test, signals_train,
-                                        words_train, words_test, semantic_features, isPGD):
-    semantic_features_map = {"Feature 1": [], "Feature 100": [], "Feature 200": []}
+                                        words_test, words_train, semantic_features,
+                                        lambda_values, is_pgd):
+
+    # Initialize data maps and the semantic features that we will test
     semantic_features_to_test = [0, 99, 199]
+    semantic_features_map_train = {0: [], 99: [], 199: []}
+    semantic_features_map_test = {0: [], 99: [], 199: []}
+    semantic_features_map_non_zero = {0: [], 99: [], 199: []}
 
     # Build the y component of the lasso algorithm for the test semantic features
     y = np.zeros((3, len(words_train)))
@@ -47,61 +69,89 @@ def plot_semantic_feature_squared_error(signals_test, signals_train,
             y[i][j] = semantic_features[word_index - 1][cur_feature]
 
     # Build the y vector for test data
-    y_test = np.zeros((3, len(words_train)))
-    for i in range(len(y)):
+    y_test = np.zeros((3, len(words_test)))
+    for i in range(len(y_test)):
         for j in range(len(words_test)):
             cur_feature = semantic_features_to_test[i]
-            word_index = words_test[j]
+            word_index = words_test[j][0]
             # Word test stores things as floats, and the lookup in semantic features doesn't work
             # unless it is converted to int
             word_index = int(word_index)
-            y_test[i][j] = semantic_features[word_index - 1][i]
+            y_test[i][j] = semantic_features[word_index - 1][cur_feature]
 
-    weights = np.zeros((3, len(signals_train[0])))
+    # For each semantic feature we want to test we will either use
+    # SCD (Stochastic Coordinate Descent) or PGD (Proximal Gradient Descent) to
+    # generate models for different tuning parameters and collect data that we
+    # will ultimately use to plot graphs
+    for i in range(len(semantic_features_to_test)):
+        for j in range(len(lambda_values)):
+            weights = np.zeros(len(signals_train[0]))
+            cur_lambda = lambda_values[j]
+            cur_semantic_feature = semantic_features_to_test[i]
+            cur_y = y[i]
+            cur_y_test = y_test[i]
+            # Based on the flag, we choose the appropriate method
+            if is_pgd:
+                weights = pgd(cur_lambda, cur_y, signals_train, weights, 20, 20)
+            else:
+                weights = scd(cur_lambda, cur_y, signals_train, weights, 20)
 
-    lambda_values = [.1, .5, 1, 5, 10, 20, 40, 100, 200]
-    results = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    results[0] = squared_error(y, signals_train, pgd(.1, y, signals_train, weights, 20, 20))
-    results[1] = squared_error(y, signals_train, pgd(.5, y, signals_train, weights, 20, 20))
-    results[2] = squared_error(y, signals_train, pgd(1, y, signals_train, weights, 20, 20))
-    results[3] = squared_error(y, signals_train, pgd(5, y, signals_train, weights, 20, 20))
-    results[4] = squared_error(y, signals_train, pgd(10, y, signals_train, weights, 20, 20))
-    results[5] = squared_error(y, signals_train, pgd(20, y, signals_train, weights, 20, 20))
-    results[6] = squared_error(y, signals_train, pgd(40, y, signals_train, weights, 20, 20))
-    results[7] = squared_error(y, signals_train, pgd(100, y, signals_train, weights, 20, 20))
-    results[8] = squared_error(y, signals_train, pgd(200, y, signals_train, weights, 20, 20))
+            # Collect performance metrics on the current model
+            squared_error_train = squared_error(cur_y, signals_train, weights)
+            squared_error_test = squared_error(cur_y_test, signals_test, weights)
+            nonzero = np.count_nonzero(weights)
 
-    plt_PGD.plot(lambda_values, results, label = "PGD")
-    plt_PGD.savefig('squaredErrorTrain_PGD_sem200.png')
-    plt_PGD.close()
+            # Add it to the data maps
+            semantic_features_map_train[cur_semantic_feature].append((math.log(cur_lambda), squared_error_train))
+            semantic_features_map_test[cur_semantic_feature].append((math.log(cur_lambda), squared_error_test))
+            semantic_features_map_non_zero[cur_semantic_feature].append((math.log(cur_lambda), nonzero))
 
-    results = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    results[0] = squared_error(y_test, signals_test, pgd(.1, y, signals_train, weights, 20, 20))
-    results[1] = squared_error(y_test, signals_test, pgd(.5, y, signals_train, weights, 20, 20))
-    results[2] = squared_error(y_test, signals_test, pgd(1, y, signals_train, weights, 20, 20))
-    results[3] = squared_error(y_test, signals_test, pgd(5, y, signals_train, weights, 20, 20))
-    results[4] = squared_error(y_test, signals_test, pgd(10, y, signals_train, weights, 20, 20))
-    results[5] = squared_error(y_test, signals_test, pgd(20, y, signals_train, weights, 20, 20))
-    results[6] = squared_error(y_test, signals_test, pgd(40, y, signals_train, weights, 20, 20))
-    results[7] = squared_error(y_test, signals_test, pgd(100, y, signals_train, weights, 20, 20))
-    results[8] = squared_error(y_test, signals_test, pgd(200, y, signals_train, weights, 20, 20))
-    print(results)
-
-    plt_PGD.plot(lambda_values, results, label = "PGD")
-    plt_PGD.savefig('squaredErrorTest_PGD_sem200.png')
-    plt_PGD.close()
-
+    # Define method and generate plot showing error and num nonzero coef
+    method = "PGD" if is_pgd else "SCD"
+    plot_squared_error(semantic_features_map_train, True, method)
+    plot_squared_error(semantic_features_map_test, False, method)
+    plot_num_zero(semantic_features_map_non_zero, method)
 
 
+# Generates a plot that shows how changes
+# in lambda affect the squared error of test and train
+# data_map - map of semantic feature to map of lambda to squared error
+# train_flag - True if plotting the training error, false if plotting test error
+# method - The method used (PGD or SCD)
+def plot_squared_error(data_map, train_flag, method):
+    for key in data_map:
+        li = data_map[key]
+        log_lambda, data = zip(*li)
+        plt.plot(log_lambda, data, label="Semantic Feature " + str(key + 1))
+    plt.xlabel("ln(lambda)")
+    plt.ylabel("Squared Error")
+    plt.legend()
+    if train_flag:
+        plt.title("Log Lambda vs. Squared Error in Training Data (For " + method + ")")
+        plt.savefig("squaredErrorTrain_" + method + ".png")
+        plt.close()
+    else:
+        plt.title("Log Lambda vs. Squared Error in Test Data (For " + method + ")")
+        plt.savefig("squaredErrorTest_" + method + ".png")
+        plt.close()
 
-# TODO: finish this function
-def plot_squared_error(y, X):
-    pass
 
-
-# TODO: finish this function
-def plot_num_zero():
-    pass
+# Generates a plot that shows how changes
+# in tuning parameter lambda affect the number of
+# nonzero coefficients in the weights vector
+# data_map - map of semantic feature to map of lambda to  num nonzero coef
+# method - The method used (PGD or SCD)
+def plot_num_zero(data_map, method):
+    for key in data_map:
+        li = data_map[key]
+        log_lambda, data = zip(*li)
+        plt.plot(log_lambda, data, label="Semantic Feature " + str(key + 1))
+    plt.xlabel("ln(lambda)")
+    plt.ylabel("Num of Zero Coefficients")
+    plt.legend()
+    plt.title("Log of Lambda vs. Number of Zero Coefficients (For " + method + ")")
+    plt.savefig("numNonZero_" + method + ".png")
+    plt.close()
 
 
 # Reads in the words train file into a vector
@@ -123,8 +173,8 @@ def squared_error(y, X, weights):
         y2[i] = np.dot(X[i], weights)
 
     diff = y-y2
-    sumError = np.sum(np.square(diff))
-    return sumError / X.shape[0]
+    sum_error = np.sum(np.square(diff))
+    return sum_error / X.shape[0]
 
 
 if __name__ == "__main__":
